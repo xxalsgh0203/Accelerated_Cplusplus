1d6de0824050d82d76a7dad560a73770f0db60da
Minho Kim <music4537@naver.com>
2021-05-21 11:26:26 +0900

    refactorzied into functions

--- Chapter3/0.cpp
+++ Chapter3/0.cpp
@@ -1,11 +1,14 @@
 #include <iomanip>
-#include <ios>
+#include <ios> // streamsize 사용
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <vector>
 
-using std::cin; using std::setprecision;
+using std::cin; using std::sort;
 using std::cout; using std::string;
-using std::endl; using std::streamsize;
+using std::endl; using std::streamsize; // 입출력 라이브러리에서 전송된 문자수나 버퍼크기를 나타내기 위해 사용
+using std::setprecision; using std::vector;
 
 int main(){
     //학생의 이름을 묻고 입력받음
@@ -22,23 +25,33 @@ int main(){
     //과제점수를 물음 
     cout << "Enter all your Homework grades, "
             "followed by end-of-file: ";
-
-    //지금까지 입력된 과제 점수의 개수와 합
-    int count = 0;
-    double sum = 0;
-
-    //입력을 위한 변수
+    vector<double> homework;
     double x;
 
-    //불변성: 지금까지 count개 점수를 입력받았으며 입력받은 점수의 합은 sum
-    while(cin >> x){
-        ++count;
-        sum += x;
+    while(cin >> x)
+        homework.push_back(x);
+
+    //과제 점수의 입력 유무를 확인
+    typedef vector<double>::size_type vec_sz;
+    vec_sz size = homework.size();
+    if(size == 0){
+        cout << endl << "You must enter your grades. "
+                            "Please try again. " << endl;
+        return 1;
     }
 
-    //결과를 출력
+    //점수를 정렬
+    sort(homework.begin(), homework.end());
+
+    //과제 점수의 중앙값을 구힘
+    vec_sz mid = size/2;
+    double median;
+    median = size%2 == 0 ? (homework[mid] + homework[mid-1]) / 2 : homework[mid];
+
+
+    //최종 점수를 구해서 출력
     streamsize prec = cout.precision();
-    cout << "Your final grade is " << setprecision(3) << 0.2 * midterm + 0.4 * final + 0.4 * sum / count << setprecision(prec) << endl;
+    cout << "Your final grade is " << setprecision(3) << 0.2 * midterm + 0.4 * final + 0.4 * median << setprecision(prec) << endl;
 
     return 0;
 }
