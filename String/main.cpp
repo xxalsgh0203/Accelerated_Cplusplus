4ec559161bbcc4e60844da0963c7352721c89265
Minho Kim <music4537@naver.com>
2021-05-24 11:40:55 +0900

    how to split strings

--- String/main.cpp
+++ String/main.cpp
@@ -1,8 +1,14 @@
 #include <vector>
 #include <string>
+#include <iostream>
+#include <cctype> // isspace 함수
 
 using std::vector;
+using std::cin;
+using std::cout;
 using std::string;
+using std::isspace;
+using std::endl;
 
 vector<string> split(const string& s){
     vector<string> ret;
@@ -37,5 +43,14 @@ int main(){
     string s;
 
     // 문자열을 한 행씩 입력받아 분할
+    while(getline(cin, s)){
+        vector<string> v = split(s);
 
+        // 벡터 v에 저장한 단어를 각각 출력
+        for(vector<string>::size_type i=0; i!=v.size(); i++){
+            cout << v[i] << endl;
+        }
+    }
+
+    return 0;
 }
